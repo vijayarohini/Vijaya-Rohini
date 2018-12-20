aaaa55fb09cf6dacbd918dae40cc0e69bc03cd02
vijayarohini <45779273+vijayarohini@users.noreply.github.com>
2018-12-20 15:01:24 +0530

    Update Alphabet.py

--- Alphabet.py
+++ Alphabet.py
@@ -1,8 +1,8 @@
 ch=('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
 n=(1,2,3,4,5,6,7,8,9)
 if(ch!=n):
-print("Alphabet")
+   print("Alphabet")
 else:
-print("No")
+   print("No")
 
       
