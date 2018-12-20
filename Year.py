8faf59f3a9edfea276e5faecd3d76a4dd305d51d
vijayarohini <45779273+vijayarohini@users.noreply.github.com>
2018-12-20 15:38:46 +0530

    Update Year.py

--- Year.py
+++ Year.py
@@ -1,6 +1,10 @@
 year=int(input(""))
-if((year%4000==0) or ((year%4==0) and (year%100!=0))):
-  print("%d is a leap year" %year)
+if(year%4==0 and year%100!=0):
+  print("leap year")
+elif(year%100==0):
+  print("no")
+elif(year%400==0):
+  print("leap year")
 else:
-  print("%d is not the leap year" %year)
+  print("no")
   
