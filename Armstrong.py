e31069c3349e15269b539850c8ad8cea840ed2d5
vijayarohini <45779273+vijayarohini@users.noreply.github.com>
2019-02-18 13:16:10 +0530

    Create Armstrong.py

---
+++ Armstrong.py
@@ -0,0 +1,12 @@
+num=int(input("Enter a number:"))  
+tmp=num
+sum=0
+while(num>0):
+    digit=num%10  
+    sum+=digit**3 
+    print(sum)
+    num//=10 
+if tmp == sum:
+    print("yes")
+else:
+    print("no")
